86463bfb4c4b9d3140c7cf015b39de69da6152cf
Michael Wu <mw10013@gmail.com>
2012-10-13 01:32:29 -0400

    fixed login form

--- src/friend_form_login/handler.clj
+++ src/friend_form_login/handler.clj
@@ -21,7 +21,9 @@
        (friend/authorize #{::user} "This page can only be seen by authenticated users."))
   (GET "/admin" request
        (friend/authorize #{::admin} "This page can only be seen by administrators."))
-  (GET "/login" [] (ring.util.response/file-response "login.html" {:root "resources"}))
+  (GET "/login" [] (-> "login.html"
+                       (ring.util.response/file-response {:root "resources"})
+                       (ring.util.response/content-type "text/html")))
   (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
   (route/not-found "Not Found"))
 
